c4a84d0f4384083bd2628f2121af62efa9bd377b
ChronoInferna <32346067+ChronoInferna@users.noreply.github.com>
2025-04-05 12:46:56 -0400

    Problem 1

--- GetStats.hpp
+++ GetStats.hpp
@@ -14,7 +14,32 @@ public:
     }
 
     int* getStats() const{
-        //write your function here!
+        Node* it = head;
+        int* output = new int[3];
+        output[0] = 0;
+        output[1] = 0;
+        output[2] = 0;
+        if (it == nullptr)
+            return output;
+
+        int sum = 0;
+        int count = 0;
+        while (it != nullptr) {
+            sum += it->data;
+            count++;
+            if (it->data > output[1]) output[1] = it->data;
+            it = it->next;
+        }
+        it = head;
+        output[0] = output[1];
+        while (it != nullptr) {
+            if (it->data < output[0]) output[0] = it->data;
+            it = it->next;
+        }
+
+        output[2] = sum / count;
+
+        return output;
     }
     
 
