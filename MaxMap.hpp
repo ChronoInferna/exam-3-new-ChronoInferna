feba385a03f75cdce4bad168e105a950f1edacc6
ChronoInferna <32346067+ChronoInferna@users.noreply.github.com>
2025-04-05 13:02:04 -0400

    Problem 3

--- MaxMap.hpp
+++ MaxMap.hpp
@@ -6,10 +6,18 @@
 #include <vector>
 
 float consultMax(const std::string& search_term, const std::map<std::string, std::vector<float>>& data) {
-    //implement your function here
+    std::vector<float> search_vec = data.at(search_term);
+    float max = 0;
+    for (float a : search_vec)
+        if (a > max) max = a;
+    
+    return max;
 }
 
 std::map<std::string, float> returnMaxMap(const std::map<std::string, std::vector<float>>& in_map) {
-    //implement your function here
-    
+    std::map<std::string, float> out_map;
+    for (auto it : in_map)
+        out_map[it.first] = consultMax(it.first, in_map);
+
+    return out_map;
 }
